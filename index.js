f580fed7b43bde58157d9532d4d826065a4da6ad
Brian Sarango <brian.sarango@baruchmail.cuny.edu>
2023-04-12 14:23:40 -0400

    complete lab

---
+++ index.js
@@ -0,0 +1,7 @@
+function divide (){     //Function divide: defined as a normal function to return 2000/1000
+    return 2000/100;
+}
+
+const square = numToSquare => numToSquare**2;   //Function square defined as a square function. Uses ** to sqaure the argument
+
+const add = (numOne, numTwo) => numOne+numTwo; //Function add defined as a square function. Takes two arguments and returns the sum of both. 
